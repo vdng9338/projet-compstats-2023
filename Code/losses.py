76945a1e69832fbbd061754a984c56d61edeca3b
InesVATI <ines.vati@eleves.enpc.fr>
2024-01-08 11:21:13 +0100

    begin train vMF (degugging)

--- Code/losses.py
+++ Code/losses.py
@@ -3,6 +3,28 @@ from typing import Any
 import torch 
 from scipy.special import iv, ive 
 
+def reconstruction_loss(model_output: torch.Tensor,
+                   edge_label_index: torch.Tensor,
+                   edge_label:torch.Tensor, 
+                   positive_weight: float = 1):
+    
+    log_sigmoid = -torch.log(1 + torch.exp(-model_output))
+    log_probs = log_sigmoid[tuple(edge_label_index)]
+    out_edge = model_output[tuple(edge_label_index)]
+    log_lik = torch.where(edge_label ==1, positive_weight*log_probs, log_probs - out_edge)
+    return log_lik.mean() # sum or mean ?
+
+# class reconstruction_loss(torch.autograd.Function):
+#     """ Reconstruction loss for vMF-VAE"""
+
+#     @staticmethod
+#     def forward(ctx, x, ind):
+#         pass
+
+#     @staticmethod
+#     def backward(ctx, grad_x, grad_ind):
+#         pass
+
 class kl_div_vmf(torch.autograd.Function):
     """ KL divergence between vMF and uniform distribution in the hypersphere"""
 
@@ -16,8 +38,9 @@ class kl_div_vmf(torch.autograd.Function):
         bessel_2 = iv(dim/2 - 1, kappas)
 
         ctx.save_for_backward(torch.tensor(dim), kappas)
-        return kappas * bessel_1/bessel_2 + ( (dim/2 - 1)*torch.log(kappas) - (dim/2)*torch.log(torch.tensor(2*torch.pi)) - torch.log(bessel_2) ) \
+        kl = kappas * bessel_1/bessel_2 + ( (dim/2 - 1)*torch.log(kappas) - (dim/2)*torch.log(torch.tensor(2*torch.pi)) - torch.log(bessel_2) ) \
             + (dim/2)*torch.log(torch.tensor(torch.pi)) + torch.log(torch.tensor(2)) - torch.lgamma(torch.tensor(dim/2))
+        return kl.mean() # sum or mean ?
         
     @staticmethod
     def backward(ctx, grad_kappa):
@@ -28,14 +51,3 @@ class kl_div_vmf(torch.autograd.Function):
 
         return None, g * grad_kappa
     
-
-class reconstruction_loss(torch.autograd.Function):
-    """ Reconstruction loss for vMF-VAE"""
-
-    @staticmethod
-    def forward(ctx, x, ind):
-        pass
-
-    @staticmethod
-    def backward(ctx, grad_x, grad_ind):
-        pass
--- Code/train_utils.py
+++ Code/train_utils.py
@@ -2,14 +2,15 @@ import torch
 from torch.optim import Adam
 from sklearn.metrics import roc_auc_score, average_precision_score
 
-def log_likelihood(adj_matrix, model_output, positive_weight):
-    log_sigmoid = -torch.log(1 + torch.exp(-model_output))
-    log_probs = torch.where(adj_matrix > 0.1, positive_weight*log_sigmoid, log_sigmoid - model_output)
-    return torch.sum(log_probs)
 
-# def kl_divergence_vMF():
-#     """ KL divergence between vMF and uniform distribution 
-#         in the hypersphere
-#     """
 
-# def train(model):
+def get_edge_probs(model_output : torch.Tensor,
+                 edge_label_index : torch.Tensor):
+    """ corresponds to labels_probs function from Victor's code """
+    nb_edges = edge_label_index.shape[-1]
+    # turn output into probs
+    out_probs = torch.sigmoid(model_output).cpu().detach()
+    # get labels
+    edge_probs = [out_probs[tuple(edge_label_index[:, i])].item() for i in range(nb_edges)]
+    
+    return edge_probs
